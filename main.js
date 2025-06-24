57c16ef19f4f10564c7f675ca6d64bf4d29b65b0
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-06-24 08:16:23 -0300

    jd

--- main.js
+++ main.js
@@ -15,7 +15,22 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivol= new Date("2029/12/30T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivol= new Date("2029-12-30T00:00:00");
 
-contadores[0].textContent = tenpoObjetivol - tempoAtual
+contaderes[0].textContent = calculatTempo(tempoObjetivol);
+
+function calculatTempo(tempoObjetivol){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivol - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60; 
+    horas %= 60;
+
+    return(dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos")
+}
+
