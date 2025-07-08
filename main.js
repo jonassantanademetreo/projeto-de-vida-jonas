07a0c42646a28c303a67072d878c574723f97f0c
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-07-08 08:15:59 -0300

    jj

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ horas %=24;
 
 
 if(tempoFinal>0){
-    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+    return [dias ,horas,minutos,segundos];
 
 }else{
     return [0, 0, 0, 0]:
